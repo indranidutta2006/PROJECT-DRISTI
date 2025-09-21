5591385536a0daefe77704ea0c02c0516ae5bfab
indranidutta2006 <indranidutta2006@gmail.com>
2025-09-21 13:41:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 # ========================= # Page Config # =========================
 st.set_page_config(page_title="Project Drishti", layout="wide")
